26a1adfe8cce258324f452d367214c72fee67243
aimerlu <1369530215@qq.com>
2020-11-23 11:22:32 +0800

    cmt

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,8 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    publicPath: "/nuxt/",
+    // publicPath: "/nuxt/",
     host: "0.0.0.0",
+    port: "3000",
   },
 };
